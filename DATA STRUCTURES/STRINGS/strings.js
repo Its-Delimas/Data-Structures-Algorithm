646e02d53c329c24edd7d034c0b08d6eedc65544
Its-Delimas <spencerdelimas@gmail.com>
2026-03-18 23:45:02 +0300

    LESSON: String and it immutability

---
+++ DATA STRUCTURES/STRINGS/strings.js
@@ -0,0 +1,17 @@
+// key things
+let s = "spencer";
+
+s.length; //7
+s[0]; //s
+s[6]; //r
+s.indexOf("1"); //2 ~ first occurences
+s.slice(1,3) //"pe", from index 1 up to(not including) 3
+s.toUpperCase() //SPENCER
+s.split(" ") //["s","p","c""..."]
+
+
+// string immutability ~ split then join
+let s ="hello";
+let arr = s.split("");
+arr[0] = "x";
+let result = arr.join("")
