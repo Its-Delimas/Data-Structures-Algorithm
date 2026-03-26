2798b77ed9732f95fe7d6ce7726b1ae1557bdc5e
Its-Delimas <spencerdelimas@gmail.com>
2026-03-26 23:07:55 +0300

    deku

--- DATA STRUCTURES/STRINGS/Q1.js
+++ DATA STRUCTURES/STRINGS/Q1.js
@@ -1,14 +1,14 @@
 // classic string problem , check if a string is a palindrome
-function isPalindrome (s){
+function isPalindrome(s) {
     let left = 0;
     let right = s.length - 1;
 
-    while (left < right ){
-        if(s[left] !== s[right]){
+    while (left < right) {
+        if (s[left] !== s[right]) {
             return false;
         }
-        left ++;
-        right --;
+        left++;
+        right--;
     }
     return true;
 }
@@ -18,4 +18,5 @@ isPalindrome('hello')
 
 //  similar this is to the reverse problem 
 // — same pointer setup, same inward movement. 
-// Only the action inside the loop changed (compare instead of swap).
+// Only the action inside the loop changed 
+// (compare instead of swap).
