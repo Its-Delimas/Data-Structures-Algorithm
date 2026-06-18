ae102a8106666d0566e486cf67aef8bf28012ee2
Its-Delimas <spencerdelimas@gmail.com>
2026-06-18 20:11:42 +0300

    fix: termination condition tied to pointers crossing

--- MODULE_2/two_pointer.ts
+++ MODULE_2/two_pointer.ts
@@ -2,7 +2,7 @@ function twoSum(arr: number[], target: number): [number, number] | null {
   let left = 0;
   let right = arr.length - 1;
 
-  while (left !== right) {
+  while (left < right) {
     let sum = arr[left] + arr[right];
     if (sum < target) {
       left++;
