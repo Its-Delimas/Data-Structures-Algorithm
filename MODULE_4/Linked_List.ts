d535f6b89521e18c6fa61e152fd2d847e51bfb7e
Its-Delimas <spencerdelimas@gmail.com>
2026-06-21 22:38:11 +0300

    feat: implement LinkedList class with append method for adding nodes

--- MODULE_4/Linked_List.ts
+++ MODULE_4/Linked_List.ts
@@ -1,9 +1,31 @@
 class ListNode {
   value: number;
   next: ListNode | null;
-
   constructor(value: number) {
     this.value = value;
     this.next = null;
+  }
+}
+class LinkedList {
+  head: ListNode | null;
+  constructor() {
+    this.head = null;
+  }
+
+  append(value: number): void {
+    const newNode = new ListNode(value);
+
+    if (this.head === null) {
+      this.head = newNode;
+      return;
+    }
+
+    let current = this.head;
+
+    while (current.next !== null) {
+      current = current.next;
+    }
+
+    current.next = newNode;
   }
 }
